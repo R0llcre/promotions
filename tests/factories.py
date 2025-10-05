d44469542904a640b96c6a322fd330ffef86c9fb
李喆宸 <leeggroy@gmail.com>
2025-10-05 05:51:44 +0000

    test: add tests for Promotion model

--- tests/factories.py
+++ tests/factories.py
@@ -3,18 +3,22 @@ Test Factory to make fake objects for testing
 """
 
 import factory
-from service.models import YourResourceModel
+from datetime import date, timedelta
+from service.models import Promotion
 
 
-class YourResourceModelFactory(factory.Factory):
-    """Creates fake pets that you don't have to feed"""
+class PromotionFactory(factory.Factory):
+    """Creates fake promotions for testing"""
 
     class Meta:  # pylint: disable=too-few-public-methods
         """Maps factory to data model"""
 
-        model = YourResourceModel
+        model = Promotion
 
     id = factory.Sequence(lambda n: n)
-    name = factory.Faker("first_name")
-
-    # Todo: Add your other attributes here...
+    name = factory.Faker("catch_phrase")
+    promotion_type = factory.Faker("random_element", elements=("Percentage off", "Buy One Get One", "Fixed amount off"))
+    value = factory.Faker("random_int", min=1, max=99)
+    product_id = factory.Faker("random_int", min=1, max=1000)
+    start_date = factory.LazyFunction(lambda: date.today())
+    end_date = factory.LazyFunction(lambda: date.today() + timedelta(days=30))
--- tests/test_models.py
+++ tests/test_models.py
@@ -15,7 +15,7 @@
 ######################################################################
 
 """
-Test cases for Pet Model
+Test cases for Promotion Model
 """
 
 # pylint: disable=duplicate-code
@@ -23,21 +23,23 @@ import os
 import logging
 from unittest import TestCase
 from wsgi import app
-from service.models import YourResourceModel, DataValidationError, db
-from .factories import YourResourceModelFactory
+from datetime import date
+from service.models import Promotion, DataValidationError, db
+from tests.factories import PromotionFactory
 
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql+psycopg://postgres:postgres@localhost:5432/testdb"
 )
 
-
 ######################################################################
-#  YourResourceModel   M O D E L   T E S T   C A S E S
+#  B A S E   T E S T   C A S E S
 ######################################################################
-# pylint: disable=too-many-public-methods
-class TestYourResourceModel(TestCase):
-    """Test Cases for YourResourceModel Model"""
 
+
+class TestCaseBase(TestCase):
+    """Base Test Case for common setup"""
+
+    # pylint: disable=duplicate-code
     @classmethod
     def setUpClass(cls):
         """This runs once before the entire test suite"""
@@ -54,26 +56,88 @@ class TestYourResourceModel(TestCase):
 
     def setUp(self):
         """This runs before each test"""
-        db.session.query(YourResourceModel).delete()  # clean up the last tests
+        db.session.query(Promotion).delete()  # clean up the last tests
         db.session.commit()
 
     def tearDown(self):
         """This runs after each test"""
         db.session.remove()
 
-    ######################################################################
-    #  T E S T   C A S E S
-    ######################################################################
+######################################################################
+#  P R O M O T I O N   M O D E L   T E S T   C A S E S
+######################################################################
+# pylint: disable=too-many-public-methods
 
-    def test_example_replace_this(self):
-        """It should create a YourResourceModel"""
-        # Todo: Remove this test case example
-        resource = YourResourceModelFactory()
-        resource.create()
-        self.assertIsNotNone(resource.id)
-        found = YourResourceModel.all()
-        self.assertEqual(len(found), 1)
-        data = YourResourceModel.find(resource.id)
-        self.assertEqual(data.name, resource.name)
 
-    # Todo: Add your test cases here...
+class TestPromotionModel(TestCase):
+    """Test Cases for Promotion Model"""
+
+    def test_serialize_a_promotion(self):
+        """It should serialize a Promotion"""
+        promotion = PromotionFactory()
+        data = promotion.serialize()
+        self.assertNotEqual(data, None)
+        self.assertIn("id", data)
+        self.assertEqual(data["id"], promotion.id)
+        self.assertIn("name", data)
+        self.assertEqual(data["name"], promotion.name)
+        self.assertIn("promotion_type", data)
+        self.assertEqual(data["promotion_type"], promotion.promotion_type)
+        self.assertIn("value", data)
+        self.assertEqual(data["value"], promotion.value)
+        self.assertIn("product_id", data)
+        self.assertEqual(data["product_id"], promotion.product_id)
+        self.assertIn("start_date", data)
+        self.assertEqual(date.fromisoformat(data["start_date"]), promotion.start_date)
+        self.assertIn("end_date", data)
+        self.assertEqual(date.fromisoformat(data["end_date"]), promotion.end_date)
+
+    def test_deserialize_a_promotion(self):
+        """It should de-serialize a Promotion"""
+        data = PromotionFactory().serialize()
+        promotion = Promotion()
+        promotion.deserialize(data)
+        self.assertNotEqual(promotion, None)
+        self.assertEqual(promotion.id, None)
+        self.assertEqual(promotion.name, data["name"])
+        self.assertEqual(promotion.promotion_type, data["promotion_type"])
+        self.assertEqual(promotion.value, data["value"])
+        self.assertEqual(promotion.product_id, data["product_id"])
+        self.assertEqual(promotion.start_date, date.fromisoformat(data["start_date"]))
+        self.assertEqual(promotion.end_date, date.fromisoformat(data["end_date"]))
+
+    def test_deserialize_missing_data(self):
+        """It should not deserialize a Promotion with missing data"""
+        data = {"id": 1, "name": "Sale", "promotion_type": "Percentage off"}
+        promotion = Promotion()
+        self.assertRaises(DataValidationError, promotion.deserialize, data)
+
+    def test_deserialize_bad_data(self):
+        """It should not deserialize bad data"""
+        data = "this is not a dictionary"
+        promotion = Promotion()
+        self.assertRaises(DataValidationError, promotion.deserialize, data)
+
+    def test_deserialize_bad_value(self):
+        """It should not deserialize a bad value attribute"""
+        test_promotion = PromotionFactory()
+        data = test_promotion.serialize()
+        data["value"] = "not_a_number"
+        promotion = Promotion()
+        self.assertRaises(DataValidationError, promotion.deserialize, data)
+
+    def test_deserialize_bad_product_id(self):
+        """It should not deserialize a bad product_id attribute"""
+        test_promotion = PromotionFactory()
+        data = test_promotion.serialize()
+        data["product_id"] = "not_a_number"
+        promotion = Promotion()
+        self.assertRaises(DataValidationError, promotion.deserialize, data)
+
+    def test_deserialize_invalid_date(self):
+        """It should not deserialize invalid date format"""
+        test_promotion = PromotionFactory()
+        data = test_promotion.serialize()
+        data["start_date"] = "invalid-date"
+        promotion = Promotion()
+        self.assertRaises(DataValidationError, promotion.deserialize, data)
