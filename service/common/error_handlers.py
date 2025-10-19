279de9f9caa4c877d0616f9732a1c318e8a1cd20
R0llcre <115418608+R0llcre@users.noreply.github.com>
2025-10-19 01:00:19 +0000

    Clarify error semantics & unify API error responses

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -6,6 +6,12 @@
 # You may obtain a copy of the License at
 #
 # https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 ######################################################################
 """
 Module: error_handlers
@@ -15,7 +21,7 @@ from flask import jsonify, make_response
 from flask import current_app as app  # Import Flask application
 from werkzeug.exceptions import MethodNotAllowed
 from service.models import DataValidationError, DatabaseError
-from . import status
+from service.common import status
 
 
 # ---------- unified response builder ----------
